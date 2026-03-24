8fb889259b6c7152eb985b7aebe7458311c3d7ac
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-03-24 11:32:09 +0545

    add basic book detail scraping (title)

--- Scrape.py
+++ Scrape.py
@@ -4,6 +4,18 @@ import requests
 base_url = "https://books.toscrape.com/"
 session = requests.Session()
 
+def get_book_details(book_url):
+    rq = session.get(book_url)
+    bs = BeautifulSoup(rq.content, "lxml")
+
+    data = {}
+
+    product_div = bs.find("div", class_="product_main")
+    if product_div:
+        data["name"] = product_div.h1.text.strip()
+
+    return data
+
 def scrape():
     url = base_url
     rq = session.get(url)
